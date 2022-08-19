ab8d46125b5637ef84730e46f2dc43fd9230f1d9
apchdotpy <106434098+apchdotpy@users.noreply.github.com>
2022-08-19 04:51:18 +0430

    Add files via upload

--- KEYS.py
+++ KEYS.py
@@ -1,4 +1,4 @@
-apikey=''
-apikeysecret=''
-accesstoken=''
-accesstokensecret=''
+apikey='API_KEY'
+apikeysecret='API_KEY_SECRET'
+accesstoken='ACCESS_TOKEN'
+accesstokensecret='ACCESS_TOKEN_SECRET'
